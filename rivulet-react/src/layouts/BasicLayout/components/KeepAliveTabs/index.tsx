e165da5343bad796a50d81f71bc46ab83cd68f6f
jqhsdtz <jqhsdtz@foxmail.com>
2022-03-02 18:50:28 +0800

    完善利用antd的Tabs组件配合react-activation组件实现的多页签功能

--- rivulet-react/src/layouts/BasicLayout/components/KeepAliveTabs/index.tsx
+++ rivulet-react/src/layouts/BasicLayout/components/KeepAliveTabs/index.tsx
@@ -4,28 +4,59 @@ import React, {useContext} from 'react';
 import {HeaderViewProps} from '@/layouts/BasicLayout/Header';
 import {headerHeight, MenuDataItem, RouteContext} from '@/layouts/BasicLayout';
 import {Tabs} from 'antd';
+import {useHistory} from 'ice';
 
 const {TabPane} = Tabs;
 
 const KeepAliveTabs: React.FC<HeaderViewProps> = () => {
-    const {getCachingNodes} = useAliveController();
+    const {getCachingNodes, drop} = useAliveController();
     const cachingNodes = getCachingNodes();
     const {matchMenuKeys} = useContext(RouteContext);
+    const history = useHistory();
     const defaultTabTitle = 'untitled';
     const [matchMenuKey] = matchMenuKeys as Array<string>;
+    const getTargetNode = targetKey => cachingNodes.find(node => {
+        const [nodeMenuData] = node.matchMenus as Array<MenuDataItem>;
+        return nodeMenuData?.key === targetKey;
+    });
+    const onChange = (targetKey) => {
+        const targetNode = getTargetNode(targetKey);
+        history.push(targetNode?.name || '');
+    }
+    const onEdit = (targetKey, action) => {
+        const targetNode = getTargetNode(targetKey);
+        const isActive = targetKey === matchMenuKey;
+        if (action === 'remove') {
+            const currentName = targetNode?.name || '';
+            if (isActive) {
+                drop(currentName).then(() => {
+                });
+                const curIdx = cachingNodes.findIndex(
+                    routeNode => routeNode.name === currentName
+                );
+                history.push(curIdx > 0 ? cachingNodes[curIdx - 1].name || '' : '');
+            } else {
+                drop(currentName).then(() => {
+                });
+            }
+        }
+    }
+    const tabClosable = cachingNodes.length > 1;
     return (
         <Tabs
             hideAdd
-            activeKey={matchMenuKey}
             type="editable-card"
             tabBarStyle={{height: headerHeight}}
+            activeKey={matchMenuKey}
+            onChange={onChange}
+            onEdit={onEdit}
         >
             {cachingNodes.map((node, index) => {
                 const [nodeMenuData] = node.matchMenus as Array<MenuDataItem>;
                 const tabTitle = nodeMenuData?.name || defaultTabTitle;
                 const tabKey = nodeMenuData?.key || index.toString();
                 return (
-                    <TabPane tab={tabTitle} key={tabKey}/>
+                    <TabPane tab={tabTitle} key={tabKey} closable={tabClosable}/>
                 )
             })}
         </Tabs>
