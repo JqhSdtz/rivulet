9baf2e79184be72699e0119d06b488b950c49717
JQH <jqhsdtz@foxmail.com>
2025-03-02 14:21:31 +0800

    提交时，将提交按钮设置为loading，防止重复提交

--- rivulet-vite/src/pages/Home/index.tsx
+++ rivulet-vite/src/pages/Home/index.tsx
@@ -3,21 +3,23 @@ import {Button} from 'antd';
 import RvRequest from '@/utils/rvRequest';
 import axios from 'axios';
 import {useRvModal} from '@/components/common/RvModal';
+import {useRequest} from 'ahooks';
+import {useState} from 'react';
 
 export default () => {
     const rvModal = useRvModal();
+    const [reqLoading, setReqLoading] = useState<boolean>(true);
     const clearScriptCache = async () => {
-        const result = await RvRequest.clearJsCache();
-        rvModal.result(result);
-    }
+        const {data, loading} = useRequest(() => RvRequest.clearJsCache());
+        setReqLoading(loading);
+        rvModal.result(data);
+    };
     return (
         <>
             <div className={styles.container}>
                 <h2>Home page</h2>
             </div>
-            <Button
-                style={{marginLeft: '1rem'}}
-                onClick={clearScriptCache}>
+            <Button style={{marginLeft: '1rem'}} onClick={clearScriptCache} loading={reqLoading}>
                 清除脚本缓存
             </Button>
         </>
